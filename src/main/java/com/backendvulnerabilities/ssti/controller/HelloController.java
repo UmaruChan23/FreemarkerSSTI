967691df3f53ee93ade7e5ef0a23d9062d1cabbe
UMRchan <f.matorin@mail.ru>
2021-08-30 22:15:02 +0300

    Рабочая версия сервиса с эксплойтом

--- src/main/java/com/backendvulnerabilities/ssti/controller/HelloController.java
+++ src/main/java/com/backendvulnerabilities/ssti/controller/HelloController.java
@@ -6,6 +6,8 @@ import freemarker.cache.StringTemplateLoader;
 import freemarker.cache.TemplateLoader;
 import freemarker.template.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +28,12 @@ public class HelloController {
 
     @RequestMapping(value = "/hello")
     public String hello(Model model) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
         model.addAttribute("object", con.getObjectWrapper());
         StringTemplateLoader stringLoader = new StringTemplateLoader();
-        stringLoader.putTemplate("hello.ftl", "Hello ");
+        stringLoader.putTemplate("hello.ftl", "Hello " + auth.getName());
         con.setTemplateLoader(new MultiTemplateLoader(new TemplateLoader[]{stringLoader,
                 con.getTemplateLoader()}));
         return "hello";
